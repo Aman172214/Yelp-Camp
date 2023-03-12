23c82d2966fd90e870941ea4db479191d44c1c69
Aman <Sharmaamqn@gmail.com>
2023-03-12 23:31:24 +0530

    add images to the corresponding campgrounds in addition to the basic styling of the website

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const Campground = require('../models/campground');
-const cities = require('./cities');
-const {places,descriptors} = require('./seedHelpers');
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelpCamp");
 
@@ -11,18 +11,23 @@ db.once("open", () => {
   console.log("Database Connected");
 });
 
-const sample = arr => arr[Math.floor(Math.random()*arr.length)]
-const seedDB = async()=>{
-    await Campground.deleteMany({});
-    for(let i=0; i<50; i++){
-        const random1000 = Math.floor(Math.random()*1000);
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
-        await camp.save();
-    }
-}
-seedDB().then(()=>{
-    mongoose.connection.close();
-})
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random()*20)+10;
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",  
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut autem quis distinctio voluptates suscipit, ducimus exercitationem vitae expedita facilis maxime, nihil ea repudiandae earum corrupti obcaecati illum laboriosam, repellat fuga.",
+      price
+    });
+    await camp.save();
+  }
+};
+seedDB().then(() => {
+  mongoose.connection.close();
+});
